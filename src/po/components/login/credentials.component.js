1469036b8f8fdc20267f5a98584eb5dc9ff6d034
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-15 16:21:21 +0500

    Add login components that extend base component

---
+++ src/po/components/login/credentials.component.js
@@ -0,0 +1,19 @@
+const BaseComponent = require('../common/base.component');
+
+class CredentialsComponent extends BaseComponent {
+  constructor() {
+    super('.login_credentials_wrap');
+  }
+
+  async validName() {
+    const name = await this.rootEl.$('.login_credentials').getText();
+    return name.split('\n')[1];
+  }
+  
+  async validPassword() {
+    const password = await this.rootEl.$('.login_password').getText();
+    return password.split('\n')[1];
+  }
+}
+
+module.exports = CredentialsComponent;
