8b94728c286b018fdc774aaa77ba0e1162cecddf
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-15 17:07:11 +0500

    Implement page factory (to make code scalable when the number of pages increases)

--- src/tests/login.tests.js
+++ src/tests/login.tests.js
@@ -1,51 +1,47 @@
 import { browser } from '@wdio/globals';
 import { Key } from 'webdriverio';
 
-const LoginPage = require("../po/pages/login.page");
-const InventoryPage = require('../po/pages/inventory.page');
-
-const loginPage = new LoginPage();
-const inventoryPage = new InventoryPage();
+const { page } = require('../po');
 
 describe('Login form', () => {
   beforeEach(async () => {
-    await loginPage.open();
+    await page('login').open();
   });
 
   it('with empty credentials', async () => {
-    await loginPage.form.input('name').setValue('John Doe');
-    await loginPage.form.input('password').setValue('Password');
+    await page('login').form.input('name').setValue('John Doe');
+    await page('login').form.input('password').setValue('Password');
 
-    await loginPage.form.input('name').click();
+    await page('login').form.input('name').click();
     await browser.keys([Key.Ctrl, 'a']);
     await browser.keys([Key.Backspace]);
-    await loginPage.form.loginBtn.click();
+    await page('login').form.loginBtn.click();
 
-    const message = await loginPage.form.errorMessage;
+    const message = await page('login').form.errorMessage;
     await expect(message).toHaveText(expect.stringContaining('Username is required'));
   });
 
   it('with credentials by passing Username', async () => {
-    await loginPage.form.input('name').setValue('John Doe');
-    await loginPage.form.input('password').setValue('Password');
+    await page('login').form.input('name').setValue('John Doe');
+    await page('login').form.input('password').setValue('Password');
 
-    await loginPage.form.input('password').click();
+    await page('login').form.input('password').click();
     await browser.keys([Key.Ctrl, 'a']);
     await browser.keys([Key.Backspace]);
-    await loginPage.form.loginBtn.click();
+    await page('login').form.loginBtn.click();
 
-    const message = await loginPage.form.errorMessage;
+    const message = await page('login').form.errorMessage;
     await expect(message).toHaveText(expect.stringContaining('Password is required'));
   });
 
   it('with credentials by passing Username & Password', async () => {
-    const name = await loginPage.credentials.validName();
-    const password = await loginPage.credentials.validPassword();
+    const name = await page('login').credentials.validName();
+    const password = await page('login').credentials.validPassword();
 
-    await loginPage.form.input('name').setValue(name);
-    await loginPage.form.input('password').setValue(password);
-    await loginPage.form.loginBtn.click();
+    await page('login').form.input('name').setValue(name);
+    await page('login').form.input('password').setValue(password);
+    await page('login').form.loginBtn.click();
 
-    await expect(await inventoryPage.header.title).toHaveText('Swag Labs');
+    await expect(await page('inventory').header.title).toHaveText('Swag Labs');
   });
 });
