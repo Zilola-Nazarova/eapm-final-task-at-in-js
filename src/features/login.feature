e57bc010a4d0014d521c8a07d0b5ec774d003376
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-19 18:46:12 +0500

    Parametrize tests to write less individual steps

--- src/features/login.feature
+++ src/features/login.feature
@@ -3,7 +3,7 @@ Feature: Login
   Scenario: As a user, I can not login without a username
 
     Given I am on the "login" page
-    When I enter any username and any password
+    When I enter "any" username and "any" password
     And I clear "username" in login form
     And I click login button in login form
     Then I should see an error message that should "contain" "Username is required"
@@ -11,7 +11,7 @@ Feature: Login
   Scenario: As a user, I can not login without a username
 
     Given I am on the "login" page
-    When I enter any username and any password
+    When I enter "any" username and "any" password
     And I clear "password" in login form
     And I click login button in login form
     Then I should see an error message that should "contain" "Password is required"
@@ -19,6 +19,6 @@ Feature: Login
   Scenario: As a user, I can not login without a username
 
     Given I am on the "login" page
-    When I enter valid username and valid password
+    When I enter "valid" username and "valid" password
     And I click login button in login form
     Then I should see an Inventory page title that should "be equal to" "Swag Labs"
--- src/step-definitions/actions-steps.js
+++ src/step-definitions/actions-steps.js
@@ -2,29 +2,21 @@ const { When, Given } = require('@wdio/cucumber-framework');
 const { Key } = require('webdriverio');
 const { page } = require('../po');
 
-Given('I am on the {string} page', function (pageName) {
+Given(/^I am on the "(\w+)" page$/, (pageName) => {
   return page(pageName).open();
 });
 
-When('I enter any username and any password', async () => {
-  await page('login').form.input('username').setValue('John Doe');
-  await page('login').form.input('password').setValue('Password');
+When(/^I enter "(valid|any)" username and "(valid|any)" password$/, async (usernameType, passwordType) => {
+  await page('login').form.input('username').setValue(usernameType === 'any' ? 'John Doe' : 'standard_user');
+  await page('login').form.input('password').setValue(passwordType === 'any' ? 'Password' : 'secret_sauce');
 });
 
-When('I enter valid username and valid password', async () => {
-  const name = await page('login').credentials.validName();
-  const password = await page('login').credentials.validPassword();
-
-  await page('login').form.input('username').setValue(name);
-  await page('login').form.input('password').setValue(password);
-});
-
-When('I clear {string} in login form', async (field) => {
+When(/^I clear "(\w+)" in login form$/, async (field) => {
   await page('login').form.input(field).click();
   await browser.keys([Key.Ctrl, 'a']);
   await browser.keys([Key.Backspace]);
 });
 
-When('I click login button in login form', function () {
+When(/^I click login button in login form$/, () => {
   return page('login').form.loginBtn.click();
 });
